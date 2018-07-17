d60e6477cc6140e0b1d6bb54b6a8f0011809bf50
Abilarsh <vabilarsh@hotmail.ca>
2018-07-17 17:25:29 -0400

    Chapter 4 Complete

--- Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch04/04_05 Begin/04_05.py
+++ Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch04/04_05 Begin/04_05.py
@@ -2,3 +2,18 @@ import numpy as np
 import cv2
 
 img = cv2.imread("faces.jpeg",1)
+grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+path = "haarcascade_frontalface_default.xml"
+face_casade = cv2.CascadeClassifier(path)
+
+faces = face_casade.detectMultiScale(grey, scaleFactor=1.08, minNeighbors=5, minSize=(42,42)) #list of all bounding boxes
+print(len(faces))
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0,255,0), 2)
+
+cv2.imshow("Image", img)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch04/04_06 Begin/04_06.py
+++ Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch04/04_06 Begin/04_06.py
@@ -2,3 +2,20 @@ import numpy as np
 import cv2
 
 img = cv2.imread("faces.jpeg",1)
+grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+path = "haarcascade_eye.xml"
+eye_casade = cv2.CascadeClassifier(path)
+
+eyes = eye_casade.detectMultiScale(grey, scaleFactor=1.01, minNeighbors=30, minSize=(8,8), maxSize=(40,40))
+print(eyes)
+
+for (x,y,w,h) in eyes:
+    xc = (x + x + w) / 2
+    yc = (y + y + h) / 2
+    radius = w / 2
+    cv2.circle(img, (int(xc), int(yc)), int(radius), (255, 0, 0), 2)
+
+cv2.imshow("Eyes", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
