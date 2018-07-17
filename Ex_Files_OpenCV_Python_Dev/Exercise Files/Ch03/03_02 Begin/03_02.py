19f05336ada5320b1e9f565a372ef862745f0057
Abilarsh <vabilarsh@hotmail.ca>
2018-07-17 00:15:59 -0400

    Chapter 3 Complete

--- Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch03/03_02 Begin/03_02.py
+++ Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch03/03_02 Begin/03_02.py
@@ -1,3 +1,24 @@
 import numpy as np
 import cv2
 
+bw  = cv2.imread("detect_blob.png",0)
+
+h, w = bw.shape[0:2]
+
+cv2.imshow("original", bw)
+
+binary = np.zeros([h, w, 1], "uint8")
+thresh = 85
+
+for row in range(h):
+    for col in range(w):
+        if bw[row][col]>thresh:
+            binary[row][col] = 255
+
+cv2.imshow("Slow Binary", binary) # faster methods exist
+
+ret, thresh = cv2.threshold(bw, 85, 255, cv2.THRESH_BINARY)
+cv2.imshow("CV Threshold", thresh)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch03/03_03 Begin/03_03.py
+++ Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch03/03_03 Begin/03_03.py
@@ -1,2 +1,15 @@
 import numpy as np
 import cv2
+
+img  = cv2.imread("sudoku.png", 0)
+
+cv2.imshow("Original", img)
+
+ret, thresh_basic = cv2.threshold(img, 70, 255, cv2.THRESH_BINARY)
+cv2.imshow("Basic Binary", thresh_basic)
+
+thresh_adapt = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 155, 1) #can dilate/erode to make better
+cv2.imshow("Adaptive Threshold", thresh_adapt)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
